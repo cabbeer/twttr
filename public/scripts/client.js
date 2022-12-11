6b6c6c232587da24e3d672837b5d4cff1e3d8406
Kabir Sewani <kabir.sewani@gmail.com>
2022-12-11 04:16:27 +0000

    implimented fetchtweetJSON function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,94 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+
+async function fetchTweetsJSON() {
+  const response = await fetch("http://localhost:8080/tweets");
+  const tweets = await response.json();
+  return tweets;
+}
+
+
+fetchTweetsJSON()
+.then((tweets) => {
+  
+  console.log(tweets)
+
+  const tweetHTML = tweets.map(
+    (tweets) => `
+      <article class="tweet-post">
+        <header>
+          <a>avatar</a>
+          <a>${tweets.user.name}</a>
+          <a>@handle</a>
+        </header>
+        <section class="tweet-post-main">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus ut mauris in aliquam. Nulla non nunc sagittis, feugiat ligula.
+        </section>
+
+        <footer>
+          <time>time of tweet</time>
+          <a><i class="fa-solid fa-flag"></i></a>
+          <a><i class="fa-solid fa-retweet"></i></a>
+          <a><i class="fa-regular fa-heart"></i></a>
+        </footer>
+      </article>    
+    `
+  )
+  .join('');
+
+console.log(tweetHTML)
+
+ 
+
+
+});
+
+
+// const renderTweets = function(tweets) {
+//   // loops through tweets
+//   // calls createTweetElement for each tweet
+//   // takes return value and appends it to the tweets container
+
+// }
+
+// const createTweetElement = function(tweet) {
+
+
+
+//       <article class="tweet-post"> 
+//         <header>
+//           <a>avatar</a>
+//           <a>@Name</a>
+//           <a>@handle</a>
+//         </header>
+//         <section class="tweet-post-main">
+//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+//           faucibus ut mauris in aliquam. Nulla non nunc sagittis, feugiat
+//           ligula.
+//         </section>
+
+//         <footer>
+//           <time>time of tweet</time>
+//           <a>
+//             <i class="fa-solid fa-flag"></i>
+//           </a>
+//           <a>
+//             <i class="fa-solid fa-retweet"></i>
+//           </a>
+//           <a>
+//             <i class="fa-regular fa-heart"></i>
+//           </a>
+//         </footer>
+//       </article>;
+
+
+//   // ...
+//   return tweet;
+// }
+
+// console.log(renderTweets(data));
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -19,26 +19,3 @@ messageEle.addEventListener("input", function (e) {
 
   
 });
-
-
-
-
-
-
-
-  // (() => {
-  //   const counter = (() => {
-  //     const input = document.getElementById("counter-input"),
-  //       display = document.getElementById("counter-display"),
-  //       changeEvent = (evt) => (display.innerHTML = evt.target.value.length),
-  //       getInput = () => input.value,
-  //       countEvent = () => input.addEventListener("keyup", changeEvent),
-  //       init = () => countEvent();
-
-  //     return {
-  //       init: init,
-  //     };
-  //   })();
-
-  //   counter.init();
-  // })();
